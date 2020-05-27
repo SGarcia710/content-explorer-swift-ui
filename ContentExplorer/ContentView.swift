78c0672c83a08a1671e6b9ee95a28803e7d19bea
SGarcia710 <sgarcia710@outlook.com>
2020-05-27 05:45:53 -0500

    Initial Commit

---
+++ ContentExplorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContentExplorer
+//
+//  Created by Sebastián on 27/05/20.
+//  Copyright © 2020 Sebastian Garcia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
