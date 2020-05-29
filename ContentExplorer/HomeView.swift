5ef9f568b0d289296f8172180d2c33d96fc85f43
SGarcia710 <sgarcia710@outlook.com>
2020-05-29 05:17:21 -0500

    15. 3D Scroll Animation

--- ContentExplorer/HomeView.swift
+++ ContentExplorer/HomeView.swift
@@ -28,9 +28,15 @@ struct HomeView: View {
             .padding(.top, 30)
             
             ScrollView(.horizontal, showsIndicators: false) { // This is the way to make an horizontal scroll.
-                HStack(spacing: 30 ) {
+                HStack(spacing: 20 ) {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                            .rotation3DEffect(Angle(degrees:
+                                Double(geometry.frame(in: .global).minX - 30) / -20
+                            ), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
                 .padding(30)
