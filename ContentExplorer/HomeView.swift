00bf2a5f307c0e4201534936152ac13fb497847b
SGarcia710 <sgarcia710@outlook.com>
2020-06-13 19:57:43 -0500

    5. If Show View and Transition

--- ContentExplorer/HomeView.swift
+++ ContentExplorer/HomeView.swift
@@ -9,171 +9,177 @@
 import SwiftUI
 
 struct HomeView: View {
-  
-  @Binding var showProfile: Bool
-  @State var showUpdate = false
-  @Binding var showContent: Bool
-  
-  var body: some View {
-    VStack {
-      HStack {
-        Text("Watching")
-          //                    .font(.system(size: 28, weight: .bold))
-          .modifier(CustomFontModifier(size: 28))
-        
-        Spacer()
-        
-        // This is the way to pass a binded state to a component.
-        AvatarView(showProfile: $showProfile)
-        
-        Button(action: { self.showUpdate.toggle() }) {
-          Image(systemName: "bell")
-            .renderingMode(.original)
-            .font(.system(size: 16, weight: .medium))
-            .frame(width: 36, height: 36)
-            .background(Color.white)
-            .clipShape(Circle())
-            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
-          
-        }
-        .sheet(isPresented: $showUpdate) {
-          UpdateList()
-        }
-      }
-        .padding(.horizontal) // 16 by default
-        .padding(.leading, 14) // We add 14 to the left side because we want the text to be align with the scroll
-        .padding(.top, 30)
-      
-      ScrollView(.horizontal, showsIndicators: false) {
-        WatchRingsView()
-          .padding(.horizontal, 30)
-          .padding(.bottom, 30)
-          .onTapGesture {
-            self.showContent = true
-        }
-      }
-      
-      
-      ScrollView(.horizontal, showsIndicators: false) { // This is the way to make an horizontal scroll.
-        HStack(spacing: 20 ) {
-          ForEach(sectionData) { item in
-            GeometryReader { geometry in
-              SectionView(section: item)
-                .rotation3DEffect(Angle(degrees:
-                  Double(geometry.frame(in: .global).minX - 30) / -20
-                ), axis: (x: 0, y: 10, z: 0))
+    
+    @Binding var showProfile: Bool
+    @State var showUpdate = false
+    @Binding var showContent: Bool
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Watching")
+                        //                    .font(.system(size: 28, weight: .bold))
+                        .modifier(CustomFontModifier(size: 28))
+                    
+                    Spacer()
+                    
+                    // This is the way to pass a binded state to a component.
+                    AvatarView(showProfile: $showProfile)
+                    
+                    Button(action: { self.showUpdate.toggle() }) {
+                        Image(systemName: "bell")
+                            .renderingMode(.original)
+                            .font(.system(size: 16, weight: .medium))
+                            .frame(width: 36, height: 36)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                        
+                    }
+                    .sheet(isPresented: $showUpdate) {
+                        UpdateList()
+                    }
+                }
+                    .padding(.horizontal) // 16 by default
+                    .padding(.leading, 14) // We add 14 to the left side because we want the text to be align with the scroll
+                    .padding(.top, 30)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    WatchRingsView()
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 30)
+                        .onTapGesture {
+                            self.showContent = true
+                    }
+                }
+                
+                
+                ScrollView(.horizontal, showsIndicators: false) { // This is the way to make an horizontal scroll.
+                    HStack(spacing: 20 ) {
+                        ForEach(sectionData) { item in
+                            GeometryReader { geometry in
+                                SectionView(section: item)
+                                    .rotation3DEffect(Angle(degrees:
+                                        Double(geometry.frame(in: .global).minX - 30) / -20
+                                    ), axis: (x: 0, y: 10, z: 0))
+                            }
+                            .frame(width: 275, height: 275)
+                        }
+                    }
+                    .padding(30)
+                        .padding(.bottom, 30) // This is because we dont want our shadow to be cutted of
+                }
+                .offset(y: -30)
+                
+                HStack{
+                    Text("Courses")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding(.leading, 30)
+                .offset(y: -60)
+                
+                SectionView(section: sectionData[2], width: screen.width - 60, height: 275)
+                    .offset(y: -60)
+                
+                Spacer() // This will push everything to the top
             }
-            .frame(width: 275, height: 275)
-          }
         }
-        .padding(30)
-        .padding(.bottom, 30) // This is because we dont want our shadow to be cutted of
-      }
-      .offset(y: -30)
-      
-      HStack{
-        Text("Courses")
-          .font(.title)
-          .bold()
-        
-        Spacer()
-      }
-      .padding(.leading, 30)
-      
-      Spacer() // This will push everything to the top
     }
-  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    // If you are binding states across files, you will have to set the defaults values for the states
-    HomeView(showProfile: .constant(false), showContent: .constant(false))
-  }
+    static var previews: some View {
+        // If you are binding states across files, you will have to set the defaults values for the states
+        HomeView(showProfile: .constant(false), showContent: .constant(false))
+    }
 }
 
 struct SectionView: View {
-  
-  var section: Section
-  var width: CGFloat = 275
-  var height: CGFloat = 275
-  
-  var body: some View {
-    VStack {
-      HStack(alignment: .top) {
-        Text(section.title)
-          .font(.system(size: 24, weight: .bold))
-          .frame(width: 160, alignment: .leading)
-          .foregroundColor(.white) // This changes the font color
-        Spacer()
-        Image(section.logo)
-      }
-      
-      Text(section.text.uppercased())
-        .frame(maxWidth: .infinity, alignment: .leading)
-      
-      section.image
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-        .frame(width: 210)
+    
+    var section: Section
+    var width: CGFloat = 275
+    var height: CGFloat = 275
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text(section.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white) // This changes the font color
+                Spacer()
+                Image(section.logo)
+            }
+            
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: width, height: height)
+        .background(section.color)
+        .cornerRadius(30)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
     }
-    .padding(.top, 20)
-    .padding(.horizontal, 20)
-    .frame(width: 275, height: 275)
-    .background(section.color)
-    .cornerRadius(30)
-    .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
-  }
 }
 
 // Data model
 struct Section: Identifiable {
-  var id = UUID()
-  var title: String
-  var text: String
-  var logo: String
-  var image: Image
-  var color: Color
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
 }
 
 let sectionData = [
-  Section(title: "Protype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card2")), color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))),
-  Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background1")), color: Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))),
-  Section(title: "SwiftUI Advanced", text: "15 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
+    Section(title: "Protype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card2")), color: Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))),
+    Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background1")), color: Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))),
+    Section(title: "SwiftUI Advanced", text: "15 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
 ]
 
 struct WatchRingsView: View {
-  var body: some View {
-    HStack(spacing: 30) {
-      HStack(spacing: 12.0) {
-        RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
-        VStack(alignment: .leading, spacing: 4.0) {
-          Text("6 minutes left").bold().modifier(FontModifier(style: .subheadline))
-          Text("Watched 10 mins today").modifier(FontModifier(style: .caption))
+    var body: some View {
+        HStack(spacing: 30) {
+            HStack(spacing: 12.0) {
+                RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
+                VStack(alignment: .leading, spacing: 4.0) {
+                    Text("6 minutes left").bold().modifier(FontModifier(style: .subheadline))
+                    Text("Watched 10 mins today").modifier(FontModifier(style: .caption))
+                }
+                
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+            
+            HStack(spacing: 12.0) {
+                RingView(color1: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1), color2: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), width: 32, height: 32, percent: 77, show: .constant(true))
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+            
+            HStack(spacing: 12.0) {
+                RingView(color1: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 32, height: 32, percent: 41, show: .constant(true))
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
         }
-        
-      }
-      .padding(8)
-      .background(Color.white)
-      .cornerRadius(20)
-      .modifier(ShadowModifier())
-      
-      HStack(spacing: 12.0) {
-        RingView(color1: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1), color2: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), width: 32, height: 32, percent: 77, show: .constant(true))
-      }
-      .padding(8)
-      .background(Color.white)
-      .cornerRadius(20)
-      .modifier(ShadowModifier())
-      
-      HStack(spacing: 12.0) {
-        RingView(color1: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 32, height: 32, percent: 41, show: .constant(true))
-      }
-      .padding(8)
-      .background(Color.white)
-      .cornerRadius(20)
-      .modifier(ShadowModifier())
     }
-  }
 }
